8359945e58966bc4a9ddf135507b710aca45c039
Klimes <klimes@janklimes.cz>
2016-02-17 16:07:50 +0100

    Signed-off-by: Klimes <klimes@janklimes.cz>

--- src/KlimesUkol1.java
+++ src/KlimesUkol1.java
@@ -3,41 +3,39 @@ import java.util.Scanner;
 public class KlimesUkol1 {
 
 	public static void main(String[] args) {
-		
-		//nacteni vstupu
+
+		// nacteni vstupu
 		System.out.println("Zadej cislo:");
 		Scanner sc = new Scanner(System.in);
 		boolean jecislo = sc.hasNextInt();
-		
-		//validace vstupu na cislo
-		if (jecislo == false)
+
+		// validace vstupu na cislo
+		if (jecislo == false) {
 			System.out.println("Neni cislo");
-		else
-		{	
+		} else {
+
 			int cislo = sc.nextInt();
-		
-		//vypise zadane cislo
-		System.out.println("Zadal jsi cislo: " + cislo);
-		
-		//vypocet
-		for (int i = 2; i < cislo; i++)
-		{
-			int vysledek = cislo/i;
-			int zbytek = cislo%i;
-			
-			System.out.println("Cislo " + cislo + " / " + i + " = " + vysledek + " zbytek = " + zbytek);
-			
-			
-			if (zbytek == 0) {
-				System.out.println("Neni prvocislo, je delitelne " + i);
-				break;
+
+			System.out.println("Zadal jsi cislo: " + cislo);
+
+			// overeni prvocisla
+			for (int i = 2; i < cislo; i++) {
+				int vysledek = cislo / i;
+				int zbytek = cislo % i;
+
+				System.out.println("Cislo " + cislo + " / " + i + " = " + vysledek + " zbytek = " + zbytek);
+
+				// pokud je zbytek 0 neni prvocislo
+				if (zbytek == 0) {
+					System.out.println("Neni prvocislo, je delitelne " + i);
+					return; // ukonci cyklus main aby se take nevypisovalo "je
+							// prvocislo"
+					// pridano v branch feature
+				}
 			}
-			
+
+			// pokud dojede az sem, je prvocislo
+			System.out.println(cislo + " je prvocislo.");
 		}
-			
-		System.out.println(cislo + " je prvocislo.");
-
 	}
-	}
-
 }
